e6b57c518bf62666b9b6f468aa2235d8a7947b97
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-07 12:44:55 +0300

    add task 2

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,7 @@ const createCategories = document.querySelector("#categories");
 const categoriesItem = [...createCategories.children];
 console.log(categoriesItem);
 console.log(`Number of categories: ${categoriesItem.length}`);
+
 categoriesItem.map((elem) => {
   const categoriesTitle = elem.querySelector("h2");
   console.log(`Category: ${categoriesTitle.textContent}`);
