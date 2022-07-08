7080a92267d94e0e48afdbfcf67570d2de6b0b61
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-08 13:12:21 +0300

    add task 5

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const refs = {
+  input: document.querySelector("#name-input"),
+  title: document.querySelector("#name-output"),
+};
+
+const defaultUserName = "Anonymous";
+
+refs.input.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  if (event.currentTarget.value === "") {
+    refs.title.textContent = defaultUserName;
+  } else {
+    refs.title.textContent = event.currentTarget.value;
+  }
+}
