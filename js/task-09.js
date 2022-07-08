a3372fdc9eeaf539089c96902c2d05371c6a857f
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-08 18:03:10 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onEventColorBtn);
+function onEventColorBtn() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
