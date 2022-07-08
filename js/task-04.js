70479db1f5620cab2616e479506b7c60ac66a0cf
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-08 12:40:31 +0300

    update task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,5 @@
 const decrementBtn = document.querySelector('[data-action="decrement"]');
-
 const value = document.querySelector("#value");
-
 const incrementBtn = document.querySelector('[ data-action="increment"]');
 
 let counterValue = 0;
@@ -9,19 +7,15 @@ let counterValue = 0;
 decrementBtn.addEventListener("click", handleDecrementClick);
 
 function handleDecrementClick() {
-  decrementBtn.addEventListener("click", () => {
-    counterValue -= 1;
-    initialValue();
-  });
+  counterValue -= 1;
+  initialValue();
 }
 
 incrementBtn.addEventListener("click", handleIncrementClick);
 
 function handleIncrementClick() {
-  incrementBtn.addEventListener("click", () => {
-    counterValue += 1;
-    initialValue();
-  });
+  counterValue += 1;
+  initialValue();
 }
 
 function initialValue() {
