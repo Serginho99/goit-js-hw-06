91e29ac98bf56647b3cb5b34bca2acb8ece20a66
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-08 17:49:00 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleEventFormEl);
+
+function handleEventFormEl(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (email === "" || password === "") {
+    alert("все поля должны быть заполнены.");
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+
+  //   const formData = new FormData(event.currentTarget);
+  //   formData.forEach((value, name) => {
+  //     console.log(value, name);
+  //   });
+  //   form.reset();
+  event.currentTarget.reset();
+}
