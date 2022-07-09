a0c221a53ba0d56b21db79e9e21489644a97961a
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-09 17:14:54 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// const controls = document.querySelector("#controls");
+// const createBtn = controls.querySelector("[data-create]");
+// const destroyBtn = controls.querySelector("[data-destroy]");
+const [input, createBtn, destroyBtn] =
+  document.querySelector("#controls").children;
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", onEventCreate);
+function onEventCreate() {
+  const arr = [];
+  for (let index = 1; index <= +input.value; index++) {
+    const wrapper = document.createElement("div");
+    wrapper.style.height = wrapper.style.width = `${30 + index * 10}px`;
+    wrapper.style.backgroundColor = getRandomHexColor();
+    arr.push(wrapper);
+  }
+  boxes.append(...arr);
+}
+
+destroyBtn.addEventListener("click", onEventDestroy);
+function onEventDestroy() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
